0c22c97c3579776d8fcea59d09060721c0ec74e3
ketangote <ketan.gote@metamagic.in>
2020-04-13 23:47:10 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { NgxSoapService, Client, ISoapMethodResponse } from 'ngx-soap';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'soap-test';
+export class AppComponent implements OnInit {
+
+  client: Client;
+  response: any;
+  constructor(private http: HttpClient, private soap: NgxSoapService) {
+    this.soap.createClient('http://uat-esb.fadv.net/ess/internal/global/SiebelFlashBoardPS?wsdl')
+      .then(client => {
+        console.log('Client', client);
+        this.client = client;
+      })
+      .catch(err => console.log('Error', err));
+
+  }
+
+  ngOnInit(){
+    const body ={
+      SRnumberSearch:{
+        SRnumberSearch:{
+          SRNumber: '1-7631330921'
+        }
+      }
+    };
+
+    this.client.call('FlashBoardSRNumberSearch_Input', body).subscribe(res => {
+      console.log(res);
+      this.response = res;
+    }, err => console.log(err));
+  }
+
+  
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgxSoapModule } from 'ngx-soap';
+import { HttpClientModule } from '@angular/common/http';
+import { AmexioWidgetModule } from "amexio-ng-extensions"; // Import Amexio library
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AmexioWidgetModule,
+    FormsModule,
+    NgxSoapModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
